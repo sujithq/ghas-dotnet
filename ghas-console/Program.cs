62bc6e979a492f80d2232039cd5749b52263dc96
Sujith Quintelier <sujith.quintelier@gmail.com>
2024-04-20 10:22:37 +0200

    Initial version

---
+++ ghas-console/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Azure.Core;
+using Azure.Security.KeyVault.Secrets;
+
+Console.WriteLine("Hello, World!");
+
+SecretClientOptions options = new()
+{
+    Retry =
+    {
+        Delay = TimeSpan.FromSeconds(2),
+        MaxRetries = 10,
+        Mode = RetryMode.Fixed
+    }
+};
