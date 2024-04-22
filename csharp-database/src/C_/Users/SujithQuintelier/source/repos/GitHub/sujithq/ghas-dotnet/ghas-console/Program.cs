37518d9b9cef9510a09be5d7bb54aa3d6ff00fcc
Sujith Quintelier <sujith.quintelier@gmail.com>
2024-04-22 08:19:54 +0200

    Update

---
+++ csharp-database/src/C_/Users/SujithQuintelier/source/repos/GitHub/sujithq/ghas-dotnet/ghas-console/Program.cs
@@ -0,0 +1,19 @@
+// See https://aka.ms/new-console-template for more information
+using Azure.Core;
+using Azure.Security.KeyVault.Secrets;
+
+Console.WriteLine("Hello, World!");
+
+const string secret="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"
+const string secret2="ghp_EqvKoXNmF4dX6oKljbZYoWRuRm1v0Z14nnA3"
+
+
+SecretClientOptions options = new()
+{
+    Retry =
+    {
+        Delay = TimeSpan.FromSeconds(2),
+        MaxRetries = 10,
+        Mode = RetryMode.Fixed
+    }
+};
